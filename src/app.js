6334bd724f88a87ada3ac17d1a5cf9308f5f228b
Pedro Borges <oi@pedroborg.es>
2018-02-17 18:34:43 -0200

    Add disabled button state

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import { h, app } from 'hyperapp'
+import cc from 'classcat'
 
 const state = {
   count: 0
@@ -11,7 +12,16 @@ const actions = {
 
 const view = (state, actions) => (
   <div class="h-full flex items-center justify-center">
-    <button class="w-8 h-8 text-grey-light" onclick={() => actions.down(1)}>
+    <button
+      class={cc([
+        'w-8 h-8 text-grey-light',
+        {
+          'cursor-not-allowed': !state.count
+        }
+      ])}
+      disabled={!state.count}
+      onclick={() => actions.down(1)}
+    >
       <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd">
         <path d="M10,20 C15.5228475,20 20,15.5228475 20,10 C20,4.4771525 15.5228475,0 10,0 C4.4771525,0 0,4.4771525 0,10 C0,15.5228475 4.4771525,20 10,20 Z M10,18 C14.418278,18 18,14.418278 18,10 C18,5.581722 14.418278,2 10,2 C5.581722,2 2,5.581722 2,10 C2,14.418278 5.581722,18 10,18 Z M15,9 L15,11 L5,11 L5,9 L15,9 Z" />
       </svg>
